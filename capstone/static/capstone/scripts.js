5f8552715576c055c6dd9cc243539bfd592b14f5
mattblas <matt.blaszkowski@gmail.com>
2022-07-20 00:21:35 +0200

    edit button

--- capstone/static/capstone/scripts.js
+++ capstone/static/capstone/scripts.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    // Use buttons to toggle between views
+    // // Use buttons to toggle between views
     document.querySelector('#add_new_route_button').addEventListener('click', () => add_new_route());
     document.querySelector('#edit_route_button').addEventListener('click', () => edit_route());
     document.querySelector('#download_score_button').addEventListener('click', () => download_score());
     document.querySelector('#do_something_button').addEventListener('click', () => do_something());
  
-    // By default, load the inbox
+    // // By default, load the inbox
     add_new_route();
   });
 
@@ -15,9 +15,6 @@ function add_new_route() {
     document.querySelector('#edit_route').style.display = 'none';
     document.querySelector('#download_score').style.display = 'none';
     document.querySelector('#do_something').style.display = 'none';
-
-    // document.querySelector('#add_new_route').innerHTML = `<h4 align="right">Add new route</h4>`;
-
 }
 
 function edit_route() {
@@ -25,8 +22,6 @@ function edit_route() {
     document.querySelector('#edit_route').style.display = 'block';
     document.querySelector('#download_score').style.display = 'none';
     document.querySelector('#do_something').style.display = 'none';
-
-    // document.querySelector('#edit_route').innerHTML = `<h4 align="right">Edit route</h4>`;
 }
 
 function download_score() {
@@ -34,8 +29,6 @@ function download_score() {
     document.querySelector('#edit_route').style.display = 'none';
     document.querySelector('#download_score').style.display = 'block';
     document.querySelector('#do_something').style.display = 'none';
-
-    // document.querySelector('#download_score').innerHTML = `<h4 align="right">Download score</h4>`;
 }
 
 function do_something() {
@@ -43,6 +36,12 @@ function do_something() {
     document.querySelector('#edit_route').style.display = 'none';
     document.querySelector('#download_score').style.display = 'none';
     document.querySelector('#do_something').style.display = 'block';
+}
 
-    // document.querySelector('#do_something').innerHTML = `<h4 align="right">Do something</h4>`;
+function edit_route_form(id) {
+    document.querySelector('#add_new_route').style.display = 'none';
+    document.querySelector('#edit_route').style.display = 'none';
+    document.querySelector('#download_score').style.display = 'none';
+    document.querySelector('#do_something').style.display = 'none';
+    document.querySelector('#edit_route_form').style.display = 'block';
 }
